4fa4933bf8378ece2ec99e02e3431679f5c344f9
wogudkim <wogudkim@gmail.com>
2021-01-11 18:25:40 +0900

    v2

--- quiz.cpp
+++ quiz.cpp
@@ -47,9 +47,12 @@ public:
 	{
 		cscore = _cscore;
 	}
+
 };
 
 int main(){
+	Student slist[10];
+	int num = 0;
 	cout << "1.  학생자료 등록" << endl
 	<< "2. 조회" << endl << "3. 성적출력" << "0. 종료(종료시 0입력)" << endl;
 
@@ -57,29 +60,48 @@ int main(){
 	while(1)
 	{
 		cin >> order;
-		if(order = 1)
+		if(order == 1)
 		{
 			string name;
 			int score;
+			cout << "학생의 이름을 입력해주세요: ";
 			cin >> name;
-			Student* s = new Student(name);
+			Student s(name) = new Student;
+			cout << "중간고사 점수 입력해주세요: ";
 			cin >> score;
 			s.setMidtermScore(score);
+			cout << "기말고사 점수 입력해주세요: ";
 			cin >> score;
 			s.setFinaltermScore(score);
+			cout << "퀴즈 점수 입력해주세요: ";
 			cin >> score;
 			s.setQuizScore(score);
+			cout << "출석 점수 입력해주세요: ";
 			cin >> score;
 			s.setCheckinScore(score);
+			slist[num] = s;
+			num++;
 		}
-		else if(order = 2)
+		else if(order == 2)
 		{
+			cout << "학생의 이름을 입력해주세요: ";
+			int name;
+			cin >> name;
+			for(int i = 0; i < 10; i++){
+				if(slist[i].name == name){
+					int total = slist[i].mscore +
+						slist[i].fscore +
+						slist[i].qscore +
+						slist[i].cscore;
+					cout << "총점: " << endl;
+				}
+			}
 			
 		}
-		else if(order = 3)
+		else if(order == 3)
 		{
 		}
-		else if(order = 0)
+		else if(order == 0)
 		{
 		}
 	}
