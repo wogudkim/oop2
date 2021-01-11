5fef2108e141abbfc7350698d391ebb193962530
wogudkim <wogudkim@gmail.com>
2021-01-11 18:40:24 +0900

    v3

--- quiz.cpp
+++ quiz.cpp
@@ -19,7 +19,7 @@ public:
 	}
 	Student(string _name)
 	{
-		name = _name
+		name = _name;
 		mscore = 0;
 		fscore = 0;
 		qscore = 0;
@@ -27,8 +27,6 @@ public:
 	}
 	~Student()
 	{
-		delete Student;
-		Student = NULL;
 	}
 
 	void setMidtermScore(int _mscore)
@@ -54,7 +52,8 @@ int main(){
 	Student slist[10];
 	int num = 0;
 	cout << "1.  학생자료 등록" << endl
-	<< "2. 조회" << endl << "3. 성적출력" << "0. 종료(종료시 0입력)" << endl;
+	<< "2. 조회" << endl << "3. 성적출력" << endl <<
+	"0. 종료(종료시 0입력)" << endl;
 
 	int order;
 	while(1)
@@ -66,7 +65,7 @@ int main(){
 			int score;
 			cout << "학생의 이름을 입력해주세요: ";
 			cin >> name;
-			Student s(name) = new Student;
+			Student s(name);
 			cout << "중간고사 점수 입력해주세요: ";
 			cin >> score;
 			s.setMidtermScore(score);
@@ -85,7 +84,7 @@ int main(){
 		else if(order == 2)
 		{
 			cout << "학생의 이름을 입력해주세요: ";
-			int name;
+			string name;
 			cin >> name;
 			for(int i = 0; i < 10; i++){
 				if(slist[i].name == name){
@@ -93,16 +92,33 @@ int main(){
 						slist[i].fscore +
 						slist[i].qscore +
 						slist[i].cscore;
-					cout << "총점: " << endl;
+					cout << "총점: " << total << endl;
+					cout << "학점: ";
+					if(total >= 90) cout << "A" << endl;
+					else if(total >= 80) cout << "B" << endl;
+					else if(total >= 70) cout << "C" << endl;
+					else if(total >= 60) cout << "D" << endl;
+					else cout << "F" << endl;
+					cout << "석차: " << i+1 <<endl;
 				}
 			}
 			
 		}
 		else if(order == 3)
 		{
+			for(int i = 0; i < 10; i++){
+				int total = slist[i].mscore +
+						slist[i].fscore +
+						slist[i].qscore +
+						slist[i].cscore;
+				cout << "순위" << i+1 << "등 총점: " << total <<
+				"이름: " << slist[i].name << endl;
+			}
 		}
 		else if(order == 0)
 		{
+			break;
 		}
+		cout << "============" << endl;
 	}
 }
